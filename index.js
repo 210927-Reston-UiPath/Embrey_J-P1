716eea8ba770bc0bf182d60d999bb4bd0bcdb3ac
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-10-20 17:23:39 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ function buy(name) {
     let productName = tableRow.insertCell(0);
     productName.innerHTML = name;
 
-    let cost = tableRow.insertCell(1);
-    price.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' .cost').innerHTML;
+    let price = tableRow.insertCell(1);
+    price.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' .price').innerHTML;
 
     let quantity = tableRow.insertCell(2);
     quantity.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' input').value;
@@ -23,3 +23,4 @@ function checkout()
     document.querySelectorAll("#cart tbody tr").forEach(row => row.remove());
     document.querySelector('#empty').innerHTML = 'No Items in cart';
 }
+
