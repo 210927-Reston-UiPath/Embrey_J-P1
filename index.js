5e92ce63543924a21d20e12aeacf9e231cff3d4c
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-10-19 23:15:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
 function buy(name) {
-    debugger;
-    // find me the element that matches this conditions (i.e a tbody child element of another element with id='cart')
-    let cart = document.querySelector('#cart tbody');
-    // appends a row to the tbody element
-    let newItem = cart.insertRow();
-
-    let itemName = newItem.insertCell(0);
-    itemName.innerHTML = name;
-
-    let elementId = '#' + name.replace(' ', '');
+    let table = document.querySelector("#cart tbody");
+    let tableRow = table.insertRow();
     
-    let itemPrice = newItem.insertCell(1);
-    itemPrice.innerHTML = document.querySelector(elementId + ' .price').innerHTML;
+    let productName = tableRow.insertCell(0);
+    productName.innerHTML = name;
 
-    let itemQuantity = newItem.insertCell(2);
-    itemQuantity.innerHTML = document.querySelector(elementId + ' td .quantity').value;
+    let price = tableRow.insertCell(1);
+    price.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' .price').innerHTML;
 
-    //resets the input value so the quantity of the original order doesn't "stick"
-    document.querySelector(elementId + ' td .quantity').value = '';
+    let quantity = tableRow.insertCell(2);
+    quantity.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' input').value;
+
+    let subTotal = tableRow.insertCell(3);
+    subTotal.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' input').value * document.querySelector('#' + name.replace(' ', '') + ' .price').innerHTML.replace('$', '');
+
+    document.querySelector('#' + name.replace(' ', '') + ' input').value = '';
+    document.querySelector('#empty').innerHTML = '';
+}
+function checkout()
+{
+    alert("thanks for your patronage!");
+    document.querySelectorAll("#cart tbody tr").forEach(row => row.remove());
+    document.querySelector('#empty').innerHTML = 'No Items in cart';
 }
