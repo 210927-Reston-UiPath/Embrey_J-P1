f253f0ec50619f28c2b9d4c834de960325ef945f
jpatemb <51932270+jpatemb@users.noreply.github.com>
2021-10-20 22:53:47 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 function buy(name) {
+    debugger;
     let table = document.querySelector("#cart tbody");
     let tableRow = table.insertRow();
     
     let productName = tableRow.insertCell(0);
     productName.innerHTML = name;
 
+    let elementId = '#' + name.replace(' ', '');
+
     let price = tableRow.insertCell(1);
-    price.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' .price').innerHTML;
+    price.innerHTML = document.querySelector(elementId + ' .price').innerHTML;
 
     let quantity = tableRow.insertCell(2);
-    quantity.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' input').value;
+    quantity.innerHTML = document.querySelector(elementId + ' input').value;
 
     let subTotal = tableRow.insertCell(3);
-    subTotal.innerHTML = document.querySelector('#' + name.replace(' ', '') + ' input').value * document.querySelector('#' + name.replace(' ', '') + ' .price').innerHTML.replace('$', '');
+    subTotal.innerHTML = document.querySelector(elementId + ' input').value * document.querySelector(elementId + ' .price').innerHTML.replace('$', '');
 
     document.querySelector('#' + name.replace(' ', '') + ' input').value = '';
     document.querySelector('#empty').innerHTML = '';
 }
+
+
 function checkout()
 {
     alert("Thank you for shopping at Embrey Enterprises! Have a wonderful day!");
     document.querySelectorAll("#cart tbody tr").forEach(row => row.remove());
     document.querySelector('#empty').innerHTML = 'No Items in cart';
 }
-
